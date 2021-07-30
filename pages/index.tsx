9686e351be42a2e867e9a9da8fbb559640ab5a32
Dany Gagnon <ddanygagnon@gmail.com>
2021-07-30 15:44:05 -0400

    feat(styles): added all default styles
    added all default styles and fixed stylelint rules.
    
    Signed-off-by: Dany Gagnon <ddanygagnon@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-    return (
-        <>
-            <h1>Je taime mon amour(Guitane)</h1>
-            <p>De dany</p>
-        </>
-    );
+    return <>Index</>;
 }
