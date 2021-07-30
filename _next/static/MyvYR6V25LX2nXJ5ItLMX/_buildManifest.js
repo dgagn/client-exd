2917c1c8132aee2db2b3c7e019196476c9e9af98
ddanygagnon <ddanygagnon@gmail.com>
2021-07-30 17:34:05 +0000

    Deploying to gh-pages from @ ddanygagnon/client-exd@f12b027d54406c7f64f4394f8deba680a786b510 🚀

--- _next/static/MyvYR6V25LX2nXJ5ItLMX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6b89f679d1dbe66df199.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],"/test":["static/chunks/pages/test-0e6014c7f7a11347af87.js"],sortedPages:["/","/_app","/_error","/test"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
