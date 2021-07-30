f12b027d54406c7f64f4394f8deba680a786b510
Dany Gagnon <ddanygagnon@gmail.com>
2021-07-30 13:33:10 -0400

    fix(index): 11y
    Signed-off-by: Dany Gagnon <ddanygagnon@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <>
-            <h1>Je taime mon amour(Guitane) ❤️</h1>
+            <h1>Je taime mon amour(Guitane)</h1>
             <p>De dany</p>
         </>
     );
