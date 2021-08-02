dc328cf7ba7d66e60797e901deb3f09d12e20024
Dany Gagnon <ddanygagnon@gmail.com>
2021-08-02 16:27:24 -0400

    chores(styles): updated my own design system in sass
    added a lot of utility classes, organized the config folder.
    
    Signed-off-by: Dany Gagnon <ddanygagnon@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
 import type { AppProps } from 'next/app';
 import '../styles/main.scss'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+import Image from 'next/image';
+import LogoCEFIR from '../public/LogoCEFIR.png';
+import Link from 'next/link';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return <>
+        <div className='container max-w-adaptive-xl mt-xl'>
+            <nav className='nav'>
+                <Image src={LogoCEFIR} />
+                <ul className='nav__list'>
+                    <li>
+                        <Link href={'/'}>Accueil</Link>
+                    </li>
+                    <li>Méthodologie</li>
+                    <Link href={'/bd'}>Base de données</Link>
+                </ul>
+            </nav>
+        </div>
+        <Component {...pageProps} />
+    </>;
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import LogoCEFIR from '../public/LogoCEFIR.png'
+
 export default function Home() {
     return <>
-        <h1 className='text-shadow-md'>Index</h1>
-        <p>allo</p>
+        <div className='container max-w-adaptive-xl'>
+            <div className='mt-md'>Accueil</div>
+        </div>
     </>;
 }
