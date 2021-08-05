459a68c1753ea6719eb5077ce7dc30a622aafddf
Dany Gagnon <ddanygagnon@gmail.com>
2021-08-05 01:43:12 -0400

    feat: image cloudinary
    Signed-off-by: Dany Gagnon <ddanygagnon@gmail.com>

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image'
 
 export default function Nav() {
     return (
             <>
                 <div className='container max-w-xl mt-xl'>
                     <nav className='nav'>
-                        <img srcSet='/LogoCEFIR.webp' alt={'Logo CEFIR'} loading='lazy' width={118} height={64} />
+                        <Image src='https://res.cloudinary.com/cefir/image/upload/v1628141832/LogoCEFIR_pjt5ic.webp' alt={'Logo CEFIR'} loading='lazy' width={118} height={64} />
                         <ul className='nav__list' aria-label='Liens de navigation principal du CEFIR'>
                             <li aria-label="Lien vers l'accueil">
                                 <Link href={'/'}>Accueil</Link>
--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
     reactStrictMode: true,
+    images: {
+        loader: 'cloudinary',
+        domains: [
+            'res.cloudinary.com'
+        ]
+    }
 };
