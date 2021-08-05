d378ba6be22ea5af6993bd04a55d4ab657ae3c54
Dany Gagnon <ddanygagnon@gmail.com>
2021-08-05 02:29:05 -0400

    fix: manifest
    Signed-off-by: Dany Gagnon <ddanygagnon@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,7 +25,7 @@ class MyDocument extends Document {
                     {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
                     {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
 
-                    <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+                    <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
                     <link rel='shortcut icon' href='/favicon.ico' />
                     <link rel="manifest" href="/manifest.json" />
                     <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
