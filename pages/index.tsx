3ba6a3238dd081cb99fbcb7aeacbbf92f2ef4f43
Dany Gagnon <ddanygagnon@gmail.com>
2021-08-10 15:04:50 -0400

    fix: performance and rerenders of home page
    Signed-off-by: Dany Gagnon <ddanygagnon@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import useStore, { StoreState } from '../store/use-store';
 import getDatabase, { Database } from '../utils/fetch-database';
-import shallow from "zustand/shallow";
+import shallow from 'zustand/shallow';
 
 const SearchInput = dynamic(() => import('../components/search-input'));
 const DatabaseTable = dynamic(() => import('../components/database/database-table'));
@@ -25,12 +25,16 @@ export async function getStaticProps() {
     };
 }
 
-const homeState = ({ filteredDatabase, loadDatabase, database }: StoreState) => ({ filteredDatabase, loadDatabase, database });
+const homeState = ({ filteredDatabase, loadDatabase, database }: StoreState) => ({
+    filteredDatabase,
+    loadDatabase,
+    database,
+});
 
 export default function Home({ database }: HomeProps) {
     const { filteredDatabase, loadDatabase, database: db } = useStore(homeState, shallow);
-    useEffect(() => { loadDatabase(database) }, [database, loadDatabase])
     console.log('render home');
+    useEffect(() => loadDatabase(database), []);
 
     return db.length > 0 ? (
         <>
@@ -43,8 +47,7 @@ export default function Home({ database }: HomeProps) {
                         <span role="img" aria-label="Un pictogramme de recherche">
                             🔍{' '}
                         </span>
-                        Rechercher - {filteredDatabase?.length}{' '}
-                        résultats
+                        Rechercher - {filteredDatabase?.length} résultats
                     </h3>
                     <p className="mt-md mb-lg" aria-label="Informations sur la recherche">
                         Par défaut, la recherche va inclure toutes les colonnes. Vous pouvez cliquer
@@ -59,6 +62,32 @@ export default function Home({ database }: HomeProps) {
                 </div>
             </div>
         </>
-    ) : null;
+    ) : (
+        <>
+            <Head>
+                <title>CEFIR - Accueil</title>
+            </Head>
+            <div className="container max-w-xl">
+                <div className="mt-2xl mb-lg">
+                    <h3>
+                        <span role="img" aria-label="Un pictogramme de recherche">
+                            🔍{' '}
+                        </span>
+                        Rechercher - {database.length} résultats
+                    </h3>
+                    <p className="mt-md mb-lg" aria-label="Informations sur la recherche">
+                        Par défaut, la recherche va inclure toutes les colonnes. Vous pouvez cliquer
+                        sur les balises pour les exclure.
+                    </p>
+
+                    <Tags />
+                    <SearchInput className={'mt-md'} />
+                    <ShowMore />
+                    <ShowMoreFilters />
+                    <DatabaseTable database={database.slice(0, 50)} className={'mb-2xl'} />
+                </div>
+            </div>
+        </>
+    );
 }
 
