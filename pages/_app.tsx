9ad184316962859cd35ca4eb771664cfd0e44aa7
Dany Gagnon <ddanygagnon@gmail.com>
2021-08-20 00:10:11 -0400

    fix: added a gap check instead of webp
    Signed-off-by: Dany Gagnon <ddanygagnon@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,9 +26,30 @@ function MyApp({ Component, pageProps }: AppProps) {
         return result
     };
 
+    const checkFlexGap = (): boolean => {
+        // create flex container with row-gap set
+        const flex = document.createElement("div");
+        flex.style.display = "flex";
+        flex.style.flexDirection = "column";
+        flex.style.rowGap = "1px";
+
+        // create two, elements inside it
+        flex.appendChild(document.createElement("div"));
+        flex.appendChild(document.createElement("div"));
+
+        // append to the DOM (needed to obtain scrollHeight)
+        document.body.appendChild(flex);
+        const isSupported = flex.scrollHeight === 1; // flex container should be 1px high from the row-gap
+        flex.parentNode!.removeChild(flex);
+        flex.remove()
+        return isSupported;
+    }
+
     useEffect(() => {
         const webpSupport = isWebPSupported();
+        const flexGap = checkFlexGap()
         !webpSupport && document.body.classList.add('no-webp')
+        !flexGap && document.body.classList.add('no-gap')
     }, [])
 
     return (
